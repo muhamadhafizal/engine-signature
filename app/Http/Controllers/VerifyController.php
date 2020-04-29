974a5b4ad1a5769b5ce7762afeafec333236655a
hafizal hussin <muhamadhafizalhussin@gmail.com>
2020-04-29 13:52:19 +0800

    new:tac verification

--- app/Http/Controllers/VerifyController.php
+++ app/Http/Controllers/VerifyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Twilio\Rest\Client;
+use App\User;
 use Illuminate\Support\Facades\Validator;
 
 class VerifyController extends Controller
@@ -16,24 +17,34 @@ class VerifyController extends Controller
 
         $validator = validator::make($request->all(),
         [
-            'phonenumber' => 'required',
+            'userid' => 'required',
         ]);
 
         if($validator->fails()){
             return response()->json($validator->errors(), 422);
         } else {
-            $phonenumber = $request->input('phonenumber');
+            $userid = $request->input('userid');
 
-            $finalformat = "'+6". $phonenumber . "'";
+            $user = User::find($userid);
 
-            $token = getenv("TWILIO_AUTH_TOKEN");
-            $twilio_sid = getenv("TWILIO_SID");
-            $twilio_verify_sid = getenv("TWILIO_VERIFY_SID");
-            $twilio = new Client($twilio_sid, $token);
-            $twilio->verify->v2->services($twilio_verify_sid)
-            ->verifications
-            ->create($finalformat, "sms");
-            return response()->json('success');
+            if($user){
+
+                $phonenumber = $user->phone;
+                $finalformat = "'+6". $phonenumber . "'";
+
+                $token = getenv("TWILIO_AUTH_TOKEN");
+                $twilio_sid = getenv("TWILIO_SID");
+                $twilio_verify_sid = getenv("TWILIO_VERIFY_SID");
+                $twilio = new Client($twilio_sid, $token);
+                $twilio->verify->v2->services($twilio_verify_sid)
+                ->verifications
+                ->create($finalformat, "sms");
+
+                return response()->json(['status'=>'success']);
+            } else {
+                return response()->json(['status'=>'failed', 'value'=>'user not exist']);
+            }
+
         }
 
     }
@@ -43,27 +54,36 @@ class VerifyController extends Controller
         $validator = validator::make($request->all(),
         [
             'taccode' => 'required',
-            'phonenumber' => 'required',
+            'userid' => 'required',
         ]);
 
         if($validator->fails()){
             return response()->json($validator->errors(), 422);
         } else {
             $taccode = $request->input('taccode');
-            $phonenumber = $request->input('phonenumber');
+            $userid = $request->input('userid');
 
-            $token = getenv("TWILIO_AUTH_TOKEN");
-            $twilio_sid = getenv("TWILIO_SID");
-            $twilio_verify_sid = getenv("TWILIO_VERIFY_SID");
-            $twilio = new Client($twilio_sid, $token);
-            $verification = $twilio->verify->v2->services($twilio_verify_sid)
-            ->verificationChecks
-            ->create($taccode, array('to' => '+60127850154'));
-            if ($verification->valid) {
-                return response()->json('process verified by phone number');
+            $user = User::find($userid);
+
+            if($user){
+
+                $phonenumber = $user->phone;
+                $finalformat = "'+6". $phonenumber . "'";
+
+                $token = getenv("TWILIO_AUTH_TOKEN");
+                $twilio_sid = getenv("TWILIO_SID");
+                $twilio_verify_sid = getenv("TWILIO_VERIFY_SID");
+                $twilio = new Client($twilio_sid, $token);
+                $verification = $twilio->verify->v2->services($twilio_verify_sid)
+                ->verificationChecks
+                ->create($taccode, array('to' => $finalformat));
+                if ($verification->valid) {
+                    return response()->json(['status'=>'success','value'=>'success verification code by phone number']);
+                }
+                return response()->json(['status'=>'failed','value'=>'Invalid verification code entered!']);
+            } else {
+                return response()->json(['status'=>'failed','value'=>'user not exist']);
             }
-            return response()->json('Invalid verification code entered!');
         }
-
     }
 }
